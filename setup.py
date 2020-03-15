3bbf005c8790f47be2e7fbb28b0f6b89efc1ffab
mathijs <resdac@outlook.com>
2020-03-15 16:12:40 +0100

    first steps towards PyPi upload

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="TransIp-Restfull-API",  # Replace with your own username
+    version="0.0.1",
+    author="Mathijs Westerhof",
+    author_email="author@example.com",
+    description="A python package to connect to the TransIp restfull API",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/mathijswesterhof/TransIp_Rest_api_python",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: GNU GPLv3 License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
